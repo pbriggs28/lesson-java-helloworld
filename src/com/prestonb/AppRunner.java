b677b3bb4dae118ee2c5afc58d42cc7270f22013
Preston Briggs <pbriggs28@users.noreply.github.com>
2017-11-12 18:14:45 -0800

    Lesson 1.2: Printing Strings

--- src/com/prestonb/AppRunner.java
+++ src/com/prestonb/AppRunner.java
@@ -3,10 +3,12 @@ package com.prestonb;
 public class AppRunner {
 
 	public void run() {
+		String robotOneName = "Hummingbird";
+		String robotTwoName = "The Dodo";
 		int robotOneScore = 300;
 		int robotTwoScore = 200;
 
-		System.out.println("Robot one score: " + robotOneScore);
-		System.out.println("Robot two score: " + robotTwoScore);
+		System.out.println(robotOneName + " score: " + robotOneScore);
+		System.out.println(robotTwoName + " score: " + robotTwoScore);
 	}
 }
