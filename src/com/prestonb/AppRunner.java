74c58f609e31e7117e544c5d783d49cc54b0ff92
Preston Briggs <pbriggs28@users.noreply.github.com>
2017-11-12 19:11:58 -0800

    Lesson 4.0: Objects. Added robots 3 and 4.

--- src/com/prestonb/AppRunner.java
+++ src/com/prestonb/AppRunner.java
@@ -5,17 +5,31 @@ public class AppRunner {
 	public void run() {
 		String robotOneName = "Hummingbird";
 		String robotTwoName = "The Dodo";
+		String robotThreeName = "Downasaur";
+		String robotFourName = "Cowsncowsncowsncows";
 		int robotOneScore = 200;
 		int robotTwoScore = 300;
+		int robotThreeScore = 285;
+		int robotFourScore = 425;
+//		int robotOneScore = 200;
+//		int robotTwoScore = 200;
+//		int robotThreeScore = 200;
+//		int robotFourScore = 200;
 		int robotOneFeetPerSec = 5;
 		int robotTwoFeetPerSec = 8;
+		int robotThreeFeetPerSec = 12;
+		int robotFourFeetPerSec = 8;
 		boolean robotOneEnabled = true;
 		boolean robotTwoEnabled = false;
+		boolean robotThreeEnabled = true;
+		boolean robotFourEnabled = true;
 
 		moveRobot(robotOneName, robotOneEnabled, robotOneFeetPerSec);
 		moveRobot(robotTwoName, robotTwoEnabled, robotTwoFeetPerSec);
+		moveRobot(robotThreeName, robotThreeEnabled, robotThreeFeetPerSec);
+		moveRobot(robotFourName, robotFourEnabled, robotFourFeetPerSec);
 		
-		judgeCompetition(robotOneName, robotOneScore, robotTwoName, robotTwoScore);
+		judgeCompetition(robotOneName, robotOneScore, robotTwoName, robotTwoScore, robotThreeName, robotThreeScore, robotFourName, robotFourScore);
 	}
 	
 	private void moveRobot(String robotName, boolean enabled, int feetPerSec) {
@@ -26,12 +40,21 @@ public class AppRunner {
 		}
 	}
 	
-	private void judgeCompetition(String robotOneName, int robotOneScore, String robotTwoName, int robotTwoScore) {
-		if(robotOneScore > robotTwoScore) {
+	private void judgeCompetition(
+			String robotOneName, int robotOneScore, 
+			String robotTwoName, int robotTwoScore,
+			String robotThreeName, int robotThreeScore, 
+			String robotFourName, int robotFourScore) {
+		
+		if(robotOneScore > robotTwoScore && robotOneScore > robotThreeScore && robotOneScore > robotFourScore) {
 			System.out.println(robotOneName + " won!");
-		} else if(robotTwoScore > robotOneScore) {
+		} else if(robotTwoScore > robotOneScore && robotTwoScore > robotThreeScore && robotTwoScore > robotFourScore) {
 			System.out.println(robotTwoName + " won!");
-		} else if(robotTwoScore == robotOneScore) {
+		} else if(robotThreeScore > robotOneScore && robotThreeScore > robotTwoScore && robotThreeScore > robotFourScore) {
+			System.out.println(robotThreeName + " won!");
+		} else if(robotFourScore > robotOneScore && robotFourScore > robotTwoScore && robotFourScore > robotThreeScore) {
+			System.out.println(robotFourName + " won!");
+		} else {
 			System.out.println("It was a tie!");
 		}
 	}
