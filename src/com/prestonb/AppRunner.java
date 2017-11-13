bdd47f75632e2637c6a6ece6ee6838c04b1ce1ec
Preston Briggs <pbriggs28@users.noreply.github.com>
2017-11-12 18:22:05 -0800

    Lesson 2.2: Checking our IF condition

--- src/com/prestonb/AppRunner.java
+++ src/com/prestonb/AppRunner.java
@@ -6,7 +6,9 @@ public class AppRunner {
 		String robotOneName = "Hummingbird";
 		String robotTwoName = "The Dodo";
 		int robotOneScore = 300;
-		int robotTwoScore = 200;
+//		int robotTwoScore = 200;
+		int robotTwoScore = 300;
+//		int robotTwoScore = 400;
 
 		System.out.println(robotOneName + " score: " + robotOneScore);
 		System.out.println(robotTwoName + " score: " + robotTwoScore);
