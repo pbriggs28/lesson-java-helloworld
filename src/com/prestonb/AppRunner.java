2e12b5a626a8addc2b8cdb5affa7e8c94e7fe090
Preston Briggs <pbriggs28@users.noreply.github.com>
2017-11-12 18:13:20 -0800

    Lesson 1.1: Printing ints

---
+++ src/com/prestonb/AppRunner.java
@@ -0,0 +1,12 @@
+package com.prestonb;
+
+public class AppRunner {
+
+	public void run() {
+		int robotOneScore = 300;
+		int robotTwoScore = 200;
+
+		System.out.println("Robot one score: " + robotOneScore);
+		System.out.println("Robot two score: " + robotTwoScore);
+	}
+}
---
+++ src/com/prestonb/Main.java
@@ -0,0 +1,9 @@
+package com.prestonb;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AppRunner app = new AppRunner();
+		app.run();
+	}
+}
